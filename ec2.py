da617cbc66f990a036efdb3da06eb17792c90a07
Matt <mhoey05@jcu.edu>
2016-07-12 16:28:06 -0700

    This worked prior to making it a flask app. Need to debug more

--- ec2.py
+++ ec2.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
 
 import boto3
-
-ec2 = boto3.resource('ec2')
+from flask import Flask
 
 client = boto3.client('ec2')
+app = Flask(__name__)
 
-instances = client.describe_instances()
+@app.route("/instances")
+def list_instances():
+	instance_array = []
+	instances = client.describe_instances()
 
-for instance in instances['Reservations'][0]['Instances']:
-	print 'Instance ID: ' + instance['InstanceId']
-	name = [ x['Value'] for x in instance['Tags'] if x['Key'] == 'Name']
-	print 'Name: ' + name[0]
+	for instance in instances['Reservations'][0]['Instances']:
+		instance_string = 'Instance ID: ' + instance['InstanceId']
+		name = [ x['Value'] for x in instance['Tags'] if x['Key'] == 'Name']
+		name_string = 'Name: ' + name[0]
+		instance_array += instance_string + name_string
+	return instance_array
+	
